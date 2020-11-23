953ec08ab7f415950a7a387c92f8fce58bc5cec3
Adam Pickering <adamkpickering@gmail.com>
2020-11-22 21:16:26 -0700

    Sorted out content-type issues and a bunch of other issues, and finally got it working

--- general/website.tf
+++ general/website.tf
@@ -15,7 +15,7 @@ terraform {
 
 locals {
   website_name = "adampickering.ca"
-  website_dir = "../../website/public"
+  website_dir  = "../../website/public"
 }
 
 provider "aws" {
@@ -41,16 +41,22 @@ resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.root.zone_id
   name    = local.website_name
   type    = "A"
-  ttl     = "300"
-  records = ["108.173.251.186"]
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.root.zone_id
   name    = "www.${local.website_name}"
   type    = "A"
-  ttl     = "300"
-  records = ["108.173.251.186"]
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
 
@@ -67,12 +73,28 @@ resource "aws_s3_bucket" "website" {
   }
 }
 
-resource "aws_s3_bucket_object" "website_file" {
-  for_each = fileset(local.website_dir, "*")
-  bucket = aws_s3_bucket.website.id
-  key = each.key
-  source = "${local.website_dir}/${each.key}"
+resource "aws_s3_bucket_object" "website_file_html" {
+  for_each     = fileset(local.website_dir, "**.html")
+  bucket       = aws_s3_bucket.website.id
+  key          = each.key
+  source       = "${local.website_dir}/${each.key}"
   content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "website_file_css" {
+  for_each     = fileset(local.website_dir, "**.css")
+  bucket       = aws_s3_bucket.website.id
+  key          = each.key
+  source       = "${local.website_dir}/${each.key}"
+  content_type = "text/css"
+}
+
+resource "aws_s3_bucket_object" "website_file_xml" {
+  for_each     = fileset(local.website_dir, "**.xml")
+  bucket       = aws_s3_bucket.website.id
+  key          = each.key
+  source       = "${local.website_dir}/${each.key}"
+  content_type = "text/xml"
 }
 
 
@@ -80,23 +102,32 @@ resource "aws_s3_bucket_object" "website_file" {
 # ACM CERTIFICATE CONFIG
 
 resource "aws_acm_certificate" "website" {
-  provider          = aws.us-east-1
-  domain_name       = "www.${local.website_name}"
-  validation_method = "DNS"
+  provider                  = aws.us-east-1
+  domain_name               = "www.${local.website_name}"
+  subject_alternative_names = toset([local.website_name])
+  validation_method         = "DNS"
 }
 
 resource "aws_route53_record" "cert" {
-  zone_id = aws_route53_zone.root.zone_id
-  name    = aws_acm_certificate.website.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.website.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.website.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.root.zone_id
 }
 
 resource "aws_acm_certificate_validation" "website" {
-  provider = aws.us-east-1
+  provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.website.arn
-  validation_record_fqdns = [aws_route53_record.cert.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
 }
 
 
@@ -128,11 +159,12 @@ resource "aws_s3_bucket_policy" "website" {
 }
 
 resource "aws_cloudfront_distribution" "website" {
-  enabled         = true
-  is_ipv6_enabled = true
-  price_class     = "PriceClass_200"
-  http_version    = "http2"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_200"
+  http_version        = "http2"
   default_root_object = "index.html"
+  aliases = [local.website_name, "www.${local.website_name}"]
 
   origin {
     domain_name = aws_s3_bucket.website.bucket_domain_name
